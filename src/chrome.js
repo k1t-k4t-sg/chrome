208b8749b8126026bd14af20bc3fa4216148df6e
Stepan <creadtive472@gmail.com>
2017-03-12 15:21:59 +1000

    MIT

--- src/chrome.js
+++ src/chrome.js
@@ -11,13 +11,16 @@
     
     
 
-$(document).ready(function(){        
-    MANDARIN.updated_page( MANDARIN.chrome_serach(), function(backMessage){
+$(document).ready(function(){
+    
+    var link = MANDARIN.chrome_serach();
+    
+    MANDARIN.updated_page(link, function(backMessage){
         
         $(OPTIONS.DOM_selector).each(function(){
             
             if(backMessage.indexOf($(this).attr("href")) !== -1){
-                $(this).parent().prepend("<span class='MANDARIN'>Тест</span>");  
+                $(this).parent().prepend("<span class='MANDARIN'>&#10026;</span>");
             }
         });
     });
