c137b695a947766abdabe0f36d457adb5b0d3e08
Stepan <creadtive472@gmail.com>
2017-03-16 22:03:53 +1000

    doc build

---
+++ build/EXPANSION.js
@@ -0,0 +1,124 @@
+;var OPTIONS = (function () {
+    
+    console.log("OPTIONS.js ==> options");
+    
+    "use strict";
+    
+    /**
+     * Настройки
+     * Настройки расширения CHROME
+     * @return {OPTIONS} type Object 
+    */
+    
+    return {
+        idExpansion: chrome.runtime.id,  //id расширения
+        DOM_selector: '.g .rc .r a'      //тег на странице google
+    }
+    
+}());
+;var MANDARIN = (function (myModel) {
+    console.log("MANDARIN.js ==> received_a_request.js"); //Получил запрос
+    
+    "use strict";
+    
+    var onConnect = chrome.runtime.onConnect;
+    
+    
+    /**
+     * Получить запрос
+     * Получает запрос onMessage и отправка ответа postMessage
+     * @param {callback} callback, function(msg, port)
+     * @return msg {object}, port {object}
+    */
+    
+    myModel.received_a_request = function (callback){
+        
+        onConnect.addListener(function(port) {
+            
+            if(port.name !== OPTIONS.idExpansion){
+                console.log("port.name !== OPTIONS.idExpansion");
+                return false
+            }
+            
+            port.onMessage.addListener(function(msg) {
+                callback(msg, port);
+            });
+        });
+    };
+    
+    return myModel;
+    
+}(MANDARIN || {}));
+;var MANDARIN = (function (myModel) {
+    console.log("MANDARIN.js ==> bookmarks_chrome.js");
+    
+    "use strict";
+    
+    var google_bookmarks; // закладки с браузера
+    
+    
+    /**
+     * Перебор вложенных закладок
+     * @param {node} Arrya закладки
+     * @return {Arrya} google_bookmarks ссылки
+    */
+    function processNode(node) {
+        if(node.children) {
+            node.children.forEach(function(child) { 
+                processNode(child); 
+            });
+        }
+        
+        if(node.url) {
+            google_bookmarks[ google_bookmarks.length ] = node.url;
+        }
+        
+    };
+    
+    /**
+     * Поиск закладок
+     * Парсинг закладок в браузере
+     * @param {callback} callback, function
+     * @return {Arrya} google_bookmarks ссылки
+    */
+    myModel.bookmarks_chrome = function(callback){
+        
+        google_bookmarks = [];
+        
+        return chrome.bookmarks.getTree(function(itemTree){
+            
+            itemTree.forEach(function(item){
+                processNode(item);
+            });
+            
+            callback(google_bookmarks);
+        });
+    }
+    
+    return myModel;
+    
+}(MANDARIN || {}));
+(function(){
+    
+    "use strict"
+    
+    // chrome expansion
+    
+    MANDARIN.received_a_request(function(msg, port){
+        MANDARIN.bookmarks_chrome(function(google_bookmarks){
+            
+            var google_bookmarks = google_bookmarks;
+            
+            var google_bookmarks_link = (msg.obj).filter(function(value, i, arr){
+                if(google_bookmarks.indexOf(value) !== -1){
+                    return value
+                }
+            });
+            
+            port.postMessage({question: google_bookmarks_link});
+            
+        });
+        
+    });
+      
+}());
